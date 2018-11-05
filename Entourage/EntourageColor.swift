000e061197075f44242284fb1c2b8246e3d8189a
liuyuxuan123 <1010134430@qq.com>
2018-11-05 10:34:16 -0500

    Change name

--- Entourage/EntourageColor.swift
+++ Entourage/EntourageColor.swift
@@ -610,47 +610,47 @@ extension UIColor{
 
 
 extension UIColor {
-    static let randomVitalColor : [UIColor] = [
+    static let randomVitalColors : [UIColor] = [
         .VitalRed,.VitalOrange,.VitalYellow,.VitalGreen,.VitalTealBlue,.VitalBlue,.VitalPurple,.VitalPink
     ]
     
-    static let randomYellowColor : [UIColor] = [
+    static let randomYellowColors : [UIColor] = [
         .MellowYellow,.CyberYellow,.RoyalYellow,.BananaYello,.TuscanyYellow,.LemonYellow,.BumblebeeYellow,.CreamYellow,.PeachYellow,.LagunaYellow,.MustardYellow,.CornYellow,.PineappleYellow,.FlaxenYellow,.EggnogYellow,.TromboneYellow,.FlaxYellow,.EcruYellow,.SepiaYellow
     ]
     
-    static let randomOrangeColor : [UIColor] = [
+    static let randomOrangeColors : [UIColor] = [
         .GoldOrange,.GoldenrodOrange,.PumpkinOrange,.FireOrange,.OchreOrange,.BurntOrange,.DijonOrange,.TangerineOrange,.TigerOrange,.HoneyOrange,.CarrotOrange,.AmberOrange,.ApricotOrange,.BronzeOrange,.CiderOrange,.ClayOrange,.RustOrange,.AmberOrange2,.SpiceOrange,
     ]
     
-    static let randomRedColor : [UIColor] = [
+    static let randomRedColors : [UIColor] = [
         .SalmonRed,.ScarletRed,.BarnRed,.ImperialRed,.IndianRed,.ChiliRed,.FireBrickRed,.MaroonRed,.RedWoodRed,.RaspberryRed,.CandyAppleRed,.FerrariRed,.PersianRed,.USFlagRed,.CarmineRed,.BurgundyRed,.CrimsonRed,.SangriaRed,.MahoganyRed
     ]
     
-    static let randomPinkColor : [UIColor] = [
+    static let randomPinkColors : [UIColor] = [
         .RubyPink,.UltraPink,.ThulianPink,.MagentaPink,.RosePink,.LavenderPink,.CreamyPink,.FuchsiaPink,.FrenchRosePink,.CerisePink,.CarnationPink,.BrickPink,.AmaranthPink,.TaffyPink,.BubbleGumPink,.HotPink,.PunchPink,.LemonadePink,.FlamingoPink
     ]
     
-    static let randomVioletColor : [UIColor] = [
+    static let randomVioletColors : [UIColor] = [
         .HibiscusViolet,.MauveViolet,.MulberryViolet,.LavenderViolet,.OrchidViolet,.LilacViolet,.ElectricViolet,.AfricanViolet,.GrapeViolet,.AmethystViolet,.ByzantineViolet,.FandangoViolet,.HelioViolet,.FloralViolet,.ThistleViolet,.RoyalViolet,.LollipopViolet,.PlumViolet,.EggplantViolet
     ]
     
-    static let randomBlueColor : [UIColor] = [
+    static let randomBlueColors : [UIColor] = [
         .YaleBlue,.PigeonBlue,.SkyBlue,.IndependenceBlue,.AirForceBlue,.BabyBlue,.NavyBlue,.SteelBlue,.CarolinaBlue,.TurkishBlue,.MayaBlue,.CornflowerBlue,.OlympicBlue,.SapphireBlue,.AzureBlue,.EgyptianBlue,.DenimBlue,.PrussianBlue,.SpaceBlue,
     ]
     
-    static let randomGreenColor : [UIColor] = [
+    static let randomGreenColors : [UIColor] = [
         .ForestGreen,.SageGreen,.OliveGreen,.LimeGreen,.HunterGreen,.JadeGreen,.ArtichokeGreen,.FernGreen,.JungleGreen,.LaurelGreen,.MossGreen,.MintGreen,.PineGreen,.TeaGreen,.ArmyGreen,.EmeraldGreen,.KellyGreen,.SacramentoGreen,.SeaGreen,
     ]
     
-    static let randomBrownColor : [UIColor] = [
+    static let randomBrownColors : [UIColor] = [
         .CedarBrown,.CinnamonBrown,.BrunetteBrown,.MochaBrown,.UmberBrown,.TortillaBrown,.ChocolateBrown,.SyrupBrown,.GingerbreadBrown,.CaramelBrown,.WalnutBrown,.PecanBrown,.WoodBrown,.HickoryBrown,.EspressoBrown,.PeanutBrown,.TawnyBrown,.CoffeeBrown,.RussetBrown,
     ]
     
-    static let randomGrayColor : [UIColor] = [
+    static let randomGrayColors : [UIColor] = [
         .FossilGray,.MinkGray,.PearlGray,.AbaloneGray,.HarborGray,.SmokeGray,.ThunderGray,.PewterGray,.SteelGray,.StoneGray,.IronGray,.RhinoGray,.TroutGray,.SealGray,.LavaGray,.ShadowGray,.AshGray,.AnchorGray,.CharcoalGray,
     ]
     
-    static let randomChineseTraditionColor : [UIColor] = [
+    static let randomChineseTraditionColors : [UIColor] = [
         .Chengdehui,.Yuanqing,.Qianzonghui,.Ganshifen,.Zitenghui,.Dianlan,.Shenzhuyue,.Tianqing,.Biyushi,.Jianghuang,.Meiguihong,.Qianjiehuang,.Qiantengzi,.Qianxieya,.Baifanghuilan,.Kongquelan,.Dailan,.Huaqing,.Meiguihui,.Shenyan,.Xiangsihui,.Zhizong,.Qiantuose,.Zongcha,.Yadanqing,.Shiyangjin,.Tenghuang,.Xiuhong,.Baicaoshuang,.Baolan,.Canghuang,.Beijingmaolab,.Canglv,
     ]
     
