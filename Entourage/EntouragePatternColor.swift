a9b89967f6e42f68acfc57669a6df7bc41b3a0e8
liuyuxuan123 <1010134430@qq.com>
2018-11-15 09:29:25 -0500

    Star Pattern Support

--- Entourage/EntouragePatternColor.swift
+++ Entourage/EntouragePatternColor.swift
@@ -134,7 +134,7 @@ extension UIColor {
             }
         case .Ring:
             let innerRadius : CGFloat = 2
-            let outterRadius : CGFloat = 4
+            let outterRadius : CGFloat = 3
             for i in 0..<numberOfRows {
                 for j in 0..<numberOfColumns{
                     let currentGridFrame = CGRect(x: CGFloat(i) * widthOfGrid,
@@ -172,8 +172,78 @@ extension UIColor {
                     backgroundPatternLayer.addSublayer(ringLayer)
                 }
             }
-        default:
-            break
+        case .Star:
+            let innerRadius : CGFloat = 2
+            let outterRadius : CGFloat = 3
+            for i in 0..<numberOfRows {
+                for j in 0..<numberOfColumns{
+                    let currentGridFrame = CGRect(x: CGFloat(i) * widthOfGrid,
+                                                  y: CGFloat(j) * heightOfGrid,
+                                                  width: widthOfGrid,
+                                                  height: heightOfGrid)
+                    
+                    //let randX = Int(arc4random_uniform(UInt32(currentGridFrame.size.width - dotRadius)))
+                    let randomXRange = outterRadius/2..<currentGridFrame.size.width-outterRadius/2
+                    let randomYRange = outterRadius/2..<currentGridFrame.size.height-outterRadius/2
+                    let randX = CGFloat.random(in: randomXRange)
+                    let randY = CGFloat.random(in: randomYRange)
+                    
+                    let starPath = UIBezierPath()
+                    let starLayer = CAShapeLayer()
+                    starLayer.frame = currentGridFrame
+                    
+                    let center = starLayer.position
+                    
+                    // Star contains 5 angles and 10 line segments
+                    let numberOfPoints = 5
+                    let numberOfLineSegments = 10
+                    let theta = CGFloat.pi / CGFloat(numberOfPoints)
+                    
+                    let circumscribedRadius = widthOfGrid / 2
+                    let outerRadius = circumscribedRadius * 1.039
+                    let excessRadius = outerRadius - circumscribedRadius
+                    let innerRadius = CGFloat(outerRadius * 0.382)  // 0.382
+                    
+                    let leftEdgePointX = (center.x + cos(4.0 * theta) * outerRadius) + excessRadius
+                    let horizontalOffset = leftEdgePointX / 2.0
+                    
+                    // Apply a slight horizontal offset so the star appears to be more
+                    // centered visually
+                    let offsetCenter = CGPoint(x: center.x - horizontalOffset, y: center.y)
+                    
+                    // Alternate between the outer and inner radii while moving evenly along the
+                    // circumference of the circle, connecting each point with a line segment
+                    for i in 0..<numberOfLineSegments {
+                        let radius = i % 2 == 0 ? outerRadius : innerRadius
+                        
+                        let pointX = randX + cos(CGFloat(i) * theta) * radius
+                        let pointY = randY + sin(CGFloat(i) * theta) * radius
+                        let point = CGPoint(x: pointX, y: pointY)
+                        
+                        if i == 0 {
+                            starPath.move(to: point)
+                        } else {
+                            starPath.addLine(to: point)
+                        }
+                    }
+                    
+                    starPath.close()
+                    
+                    // Rotate the path so the star points up as expected
+                    var pathTransform  = CGAffineTransform.identity
+                    pathTransform = pathTransform.translatedBy(x: center.x, y: center.y)
+                    pathTransform = pathTransform.rotated(by: CGFloat(-.pi / 2.0))
+                    pathTransform = pathTransform.translatedBy(x: -center.x, y: -center.y)
+                    
+                    starPath.apply(pathTransform)
+                    
+                    starLayer.path = starPath.cgPath
+                    starLayer.frame = currentGridFrame
+                    starLayer.fillColor = colors.sample?.cgColor
+                    starLayer.backgroundColor = UIColor.clear.cgColor
+                    backgroundPatternLayer.addSublayer(starLayer)
+                }
+            }
         }
         // Currently don't require anything about UIView's frame
         UIGraphicsBeginImageContextWithOptions(patternFrame.size, false, 0.0)
